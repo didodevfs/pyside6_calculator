c60ff236c8f186038ccb92c149e275eba38e438c
Felipe Diniz <felipedinizse@gmail.com>
2026-04-26 18:34:57 -0300

    just a detail: adding '.png' to the icon path

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-ICON_PATH = 'C:\\Users\\felip\\OneDrive\\Documentos\\Programação\\Curso_Udemy\\Seção 7 - Interface Gráfica\\aula202-calculadora\\files\\icons\\calc_icon4'
+ICON_PATH = 'C:\\Users\\felip\\OneDrive\\Documentos\\Programação\\Curso_Udemy\\Seção 7 - Interface Gráfica\\aula202-calculadora\\files\\icons\\calc_icon4.png'
 
 def reseting_win_attributes(self):
     self.equation = ''
